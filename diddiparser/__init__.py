d9fb437e14dc6929bce989bf968a17e4232ccfb9
Diego Ramirez <dr01191115@gmail.com>
2021-06-02 09:23:49 -0500

    Update the __version__ variable
    Upgrade to 1.1.1.

--- diddiparser/__init__.py
+++ diddiparser/__init__.py
@@ -6,5 +6,5 @@ It uses the syntax described in GRAMMAR.md.
 By Diego Ramirez (2015-2021).
 """
 
-__version__ = "1.0.0"
+__version__ = "1.1.1"
 __author__ = "Diego Ramirez (dr01191115@gmail.com) @DiddiLeija on GitHub"
