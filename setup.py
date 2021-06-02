0587257818d6f369de35484b6087eb0b9ce87249
Diego Ramirez <dr01191115@gmail.com>
2021-06-02 09:23:13 -0500

    Update the version argument
    Now, start to develop version 1.1.1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="diddiparser",
-    version="1.1.0",
+    version="1.1.1",
     author="Diego Ramirez",
     author_email="dr01191115@gmail.com",
     description="Parser for DiddiScript files.",
