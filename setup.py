38f1a13024bcd528246a088f7d22f514ea9964da
Diego Ramirez <dr01191115@gmail.com>
2021-08-16 09:17:23 -0500

    Upgrade to 1.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="diddiparser",
-    version="1.1.1",
+    version="1.2.1",
     author="Diego Ramirez",
     author_email="dr01191115@gmail.com",
     description="Parser for DiddiScript files.",
