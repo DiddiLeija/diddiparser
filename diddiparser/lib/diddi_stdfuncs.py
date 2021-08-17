b39dec6101ef3fb3ffaee221ad64e4bf819f2e1e
Diego Ramirez <dr01191115@gmail.com>
2021-08-17 10:52:59 -0500

    Update diddi_stdfuncs.py
    Remove a superseeded function.

--- diddiparser/lib/diddi_stdfuncs.py
+++ diddiparser/lib/diddi_stdfuncs.py
@@ -3,31 +3,13 @@
 import os
 import sys
 import traceback
-import warnings
 import subprocess
 import shlex
-from typing import Optional, Dict
+from typing import Optional
 
 # this only works under Windows systems,
 # see https://github.com/DiddiLeija/diddiparser/issues/6
 from os import startfile
-
-
-def pyrun(line: str, py_locals: Dict[str, str]) -> None:
-    "equivalent of the DiddiScript `pyrun()`"
-    try:
-        line = line.lstrip().replace(");", "").replace("'", "")
-        exec(line[len("pyrun "):len(line)-1], py_locals)
-    except Warning as w:
-        warnings.warn(str(w), type(w))
-    except Exception:
-        exc_type, value, tb = sys.exc_info()
-        sys.last_type = type
-        sys.last_value = value
-        sys.last_traceback = tb
-        traceback.print_exception(exc_type, value, sys.last_traceback)
-        print()
-    return None
 
 
 def ramz_goto(line: str) -> Optional[str]:
