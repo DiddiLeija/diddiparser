290a2c02d793ca8c2f346121401a04240e70feff
Diego Ramirez <dr01191115@gmail.com>
2021-08-17 10:33:50 -0500

    Update diddi_extensions.py

--- examples/extensions_usage/diddi_extensions.py
+++ examples/extensions_usage/diddi_extensions.py
@@ -2,21 +2,11 @@
 
 from diddiparser.parser import def_func
 
-_NAME = "Unknown"
-
 
 def ask(s: str) -> None:
     "ask for a name and save it."
-    _NAME = input(s)
+    name = input(s)
+    print(f"Hello, {name}. I am DiddiScript.")
 
 
-def response(s: str) -> None:
-    "use the name on your string 's'."
-    try:
-        print(s % _NAME)
-    except Exception as e:
-        print(f"ERROR: Could not print response: {str(e)}")
-
-
-def_func("ask_for_name", ask)
-def_func("say_with_name", response)
+def_func("ask_for_name", ask_and_response)
