948c394e62ee9ecbaa23199b674b3561a4a81b74
Diego Ramirez <dr01191115@gmail.com>
2021-08-17 09:41:31 -0500

    Update diddi_extensions.py
    Make flake8 fixes.

--- examples/extensions_usage/diddi_extensions.py
+++ examples/extensions_usage/diddi_extensions.py
@@ -2,18 +2,21 @@
 
 from diddiparser.parser import def_func
 
-NAME = "Unknown"
+_NAME = "Unknown"
+
 
 def ask(s: str) -> None:
     "ask for a name and save it."
-    NAME = input(s)
+    _NAME = input(s)
+
 
 def response(s: str) -> None:
     "use the name on your string 's'."
     try:
-        print(s%NAME)
-    except:
-        print(s)
+        print(s % _NAME)
+    except Exception as e:
+        print(f"ERROR: Could not print response: {str(e)}")
+
 
 def_func("ask_for_name", ask)
 def_func("say_with_name", response)
