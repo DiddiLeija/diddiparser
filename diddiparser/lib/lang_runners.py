dd7ada773d5917c5f3f7807644437d67bebdf5ad
Diego Ramirez <dr01191115@gmail.com>
2021-08-17 10:49:34 -0500

    Create lang_runners.py
    Move "pyrun()" here.

---
+++ diddiparser/lib/lang_runners.py
@@ -0,0 +1,22 @@
+"Run code from other languages."
+
+import traceback
+import sys
+import warnings
+
+
+def pyrun(line: str, py_locals: Dict[str, str]) -> None:
+    "equivalent of the DiddiScript `pyrun()`"
+    try:
+        line = line.lstrip().replace(");", "").replace("'", "")
+        exec(line[len("pyrun "):len(line)-1], py_locals)
+    except Warning as w:
+        warnings.warn(str(w), type(w))
+    except Exception:
+        exc_type, value, tb = sys.exc_info()
+        sys.last_type = type
+        sys.last_value = value
+        sys.last_traceback = tb
+        traceback.print_exception(exc_type, value, sys.last_traceback)
+        print()
+    return None
