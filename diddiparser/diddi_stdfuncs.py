f4264e5deae78b219fd034f04bea235ae6116a44
Diego Ramirez <dr01191115@gmail.com>
2021-08-17 09:47:07 -0500

    Update diddi_stdfuncs.py

--- diddiparser/diddi_stdfuncs.py
+++ diddiparser/diddi_stdfuncs.py
@@ -12,14 +12,15 @@ from typing import Optional, Dict
 # see https://github.com/DiddiLeija/diddiparser/issues/6
 from os import startfile
 
+
 def pyrun(line: str, py_locals: Dict[str, str]) -> None:
     "equivalent of the DiddiScript `pyrun()`"
     try:
         line = line.lstrip().replace(");", "").replace("'", "")
         exec(line[len("pyrun "):len(line)-1], py_locals)
-    except Warning as e:
-        warnings.warn(str(e), type(e))
-    except Exception as e:
+    except Warning as w:
+        warnings.warn(str(w), type(w))
+    except Exception:
         exc_type, value, tb = sys.exc_info()
         sys.last_type = type
         sys.last_value = value
@@ -28,20 +29,24 @@ def pyrun(line: str, py_locals: Dict[str, str]) -> None:
         print()
     return None
 
+
 def ramz_goto(line: str) -> Optional[str]:
     "equivalent of `ramz_goto()`"
     line = line.lstrip().replace(");", "").replace("'", "")
     path = line[len("ramz_goto("):len(line)-1]
-    if os.path.exists(f"c:/program files/ramz editions/{path.lower()}/build/exe.win32-3.8/{path.lower()}.exe"):
+    expected = "c:/program files/ramz editions/%s/build/exe.win32-3.8/%s.exe"
+    expected = expected % (path.lower(), path.lower())
+    if os.path.exists(expected):
         # it is hosted on "C:/Program Files/Ramz Editions"
-        startfile(f"c:/program files/ramz editions/{path.lower()}/build/exe.win32-3.8/{path.lower()}.exe")
-    elif os.path.exists(f"c:/program files/{path.lower()}/.ramz/ramz.diddi") and os.path.exists(f"c:/program files/{path.lower()}/build/exe.win32-3.8/{path.lower()}.exe"):
+        startfile(expected)
+    elif (os.path.exists(f"c:/program files/{path.lower()}/.ramz/ramz.diddi") and
+          os.path.exists(f"c:/program files/{path.lower()}/build/exe.win32-3.8/{path.lower()}.exe")):
         return "USE_SETUP"
     else:
         # build a safe exception
         try:
             raise FileNotFoundError(f"Ramz Ed. app '{path}' does not exists or it is not a Ramz Ed. product")
-        except:
+        except Exception:
             exc_type, value, tb = sys.exc_info()
             sys.last_type = exc_type
             sys.last_value = value
@@ -50,19 +55,21 @@ def ramz_goto(line: str) -> Optional[str]:
             print()
     return None
 
+
 def openfile(line: str) -> None:
     line = line.lstrip().replace(");", "").replace("'", "")
     line = line[len("openfile "):len(line)-1]
     try:
         startfile(line) # try not to move this func
         print(f"Done opening {line}")
-    except Exception as e:
+    except Exception:
         exc_type, value, tb = sys.exc_info()
         sys.last_type = type
         sys.last_value = value
         sys.last_traceback = tb
         traceback.print_exception(exc_type, value, sys.last_traceback)
         print()
+
 
 def subprocess_run(line: str) -> None:
     line = line.lstrip().replace(");", "").replace("'", "")
