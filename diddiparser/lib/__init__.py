dd0fe4adb751847554dfe95843a7a6985870f3c5
Diego Ramirez <dr01191115@gmail.com>
2021-08-20 21:33:25 -0500

    Update __init__.py
    Extend the import statement from "diddiparser.lib.*"...

--- diddiparser/lib/__init__.py
+++ diddiparser/lib/__init__.py
@@ -1,1 +1,11 @@
 "Standard lib for DiddiScript."
+
+from diddiparser.lib.lang_runners import *
+from diddiparser.lib.diddi_stdfuncs import *
+
+from diddiparser.lib.lang_runners import lang_runners_all
+from diddiparser.lib.diddi_stdfuncs import diddi_stdfuncs_all
+
+__all__ = lang_runners_all + diddi_stdfuncs_all
+
+del(lang_runners_all, diddi_stdfuncs_all)
