1d036c78793fb7d6213a0862d50b81d941b13c0f
Diego Ramirez <dr01191115@gmail.com>
2021-08-21 07:43:53 -0500

    Update __init__.py
    Modify the imports.

--- diddiparser/lib/__init__.py
+++ diddiparser/lib/__init__.py
@@ -1,11 +1,19 @@
 "Standard lib for DiddiScript."
 
-from diddiparser.lib.lang_runners import *
-from diddiparser.lib.diddi_stdfuncs import *
+from diddiparser.lib import lang_runners
+from diddiparser.lib import diddi_stdfuncs
 
-from diddiparser.lib.lang_runners import lang_runners_all
-from diddiparser.lib.diddi_stdfuncs import diddi_stdfuncs_all
+from diddiparser.lib.lang_runners import __all__ as lang_runners_all
+from diddiparser.lib.diddi_stdfuncs import __all__ as diddi_stdfuncs_all
 
 __all__ = lang_runners_all + diddi_stdfuncs_all
 
-del(lang_runners_all, diddi_stdfuncs_all)
+# add here the known functions
+STD_FUNCS = tuple(__all__)
+
+KNOWN_FUNCS = {"pyrun": lang_runners.pyrun,
+               "ramz_goto": diddi_stdfuncs.ramz_goto,
+               "openfile": diddi_stdfuncs.openfile,
+               "subprocess_run": diddi_stdfuncs.subprocess_run}
+
+__all__.append("KNOWN_FUNCS")
